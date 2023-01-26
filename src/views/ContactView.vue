29cc787b321a00e62a3fc684e2552c026800b120
julio <juliovilalba.dsn.erp@alterdata.com.br>
2023-01-26 09:57:17 -0300

    Retirada do link do Facebook

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -4,10 +4,7 @@
         <ul>
           <li>
             <a href="https://linktr.ee/aparecidamiranda" target="_blank"><i class="fa-brands fa-whatsapp fa-2xl"></i></a>
-          </li> 
-          <li>
-            <a href="https://www.facebook.com/draparecidamiranda" target="_blank"><i class="fa-brands fa-facebook fa-2xl"></i></a>              
-          </li> 
+          </li>           
           <li>
             <a href="https://instagram.com/aparecidamirandapsicologa?utm_medium=copy_link" target="_blank"><i class="fa-brands fa-instagram fa-2xl"></i></a>
           </li>
