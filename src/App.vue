433bb5a203de09d4f763f1393450bf9e2a4fbccd
julio <juliovilalba.dsn.erp@alterdata.com.br>
2023-02-02 14:03:02 -0300

    Ajuste layout

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
 <template>
-  <div id="app">    
-    <nav>     
-      <router-link to="/">Principal</router-link> 
-      <router-link to="/portfolio">Sobre</router-link> 
-      <router-link to="/contact">Contatos</router-link> 
-    </nav>
+  <div id="app">   
     <router-view />
   </div>
 </template>
@@ -17,14 +12,6 @@ export default {
 </script>
 
 <style>
-body {
-  font-family: "PT Sans", sans-serif;
-  font-size: 13px;
-  font-weight: 400;
-  color: #4f5d6e;
-  position: relative;  
-  background: linear-gradient(to right, #8766c4 , #b19cd9);  
-}
 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -32,34 +19,6 @@ body {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;  
-}
-
-nav {
-  padding: 20px;  
-  margin-bottom: 30px;
-  box-shadow: 5px 4px 5px #000;
-  background-color: #b19cd9;  
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-  text-decoration: none;  
-  padding: 10px;
-  margin: 10px;
-}
-
-nav a.router-link-exact-active {
-  color: #8766c4;
-}
-
-h1, h2 {  
-  font-family: 'Courier New', Courier, monospace;
-}
-
-
-h2 {
-  font-size: medium;
 }
 
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,201 @@
 <template>
-  <div class="home">    
+  <div class="home">
+    <div class="line"></div>
+    <div class="page-title">
+      <div>
+        <h1>Aparecida Miranda</h1>
+        <h2>Psicóloga</h2>
+      </div>
+      <div class="line"></div>
+    </div>
     <img class="foto" src="../assets/foto2.jpg" />
-    <h1>Aparecida Miranda</h1>
-    <h2>Psicóloga</h2>        
+    <div class="line"></div>
+    <div class="about">
+      <h2>Sobre</h2>          
+      <ul class="lista-portfolio">
+        <li class="item-lista-portfolio">
+          Psicóloga Clínica e Esportiva - CRP 05/42778.
+        </li>
+        <li class="item-lista-portfolio">
+          Pedagoga.
+        </li>
+        <li class="item-lista-portfolio">
+          Especialista em Psicoterapia de Família e Casal pela PUC-RJ.
+        </li>
+        <li class="item-lista-portfolio">
+          Especialista em Psicologia e Neurociências do Esporte pelo IBMR / Laureate International
+          Universities.
+        </li>
+        <li class="item-lista-portfolio">
+          Terapeuta Cognitivo Comportamental pelo IBH.
+        </li>
+        <li class="item-lista-portfolio">Coach pela Academia Emocional.</li>
+        <li class="item-lista-portfolio">
+          Atende pacientes com transtornos de ansiedade, transtornos psicóticos,
+          problemas de relacionamentos, transtornos do controle dos impulsos,
+          transtornos dissociativos, transtornos de adaptação, transtornos do
+          humor, transtornos da personalidade, transtornos da aprendizagem.
+        </li>
+        <li class="item-lista-portfolio">
+          Atua como orientadora vocacional e profissional com adolescentes em
+          fase da primeira escolha profissional e jovens adultos em reorientação
+          profissional.
+        </li>
+        <li class="item-lista-portfolio">
+          Trabalha com atletas de alto rendimento de diversas modalidades, desde
+          2010.
+        </li>
+      </ul>
+    </div>
+    <div class="line"></div>
+    <div class="book">
+      <h2>Publicação</h2> 
+      <img class="foto2" src="../assets/livro.jpg" />  
+      <p>
+        <strong>
+          Sinopse
+        </strong>
+      </p>              
+      <p>
+        &nbsp; &nbsp; A Filha de Onélia oferece-nos sua história de vida como ponto de partida para um diálogo sobre vivências comuns à muitas mulheres. Ao revisitar o passado, mais propriamente a história da mulher, ela debate como a construção de um lugar de igualdade e autonomia para a mulher foi um processo de muitas lutas que possibilitaram que pudesse estudar, trabalhar e fazer escolhas, para além da maternidade e do casamento.
+      </p>
+      <p>
+        &nbsp; &nbsp; No livro A Filha de Onélia: a visão de uma mulher na contemporaneidade, Aparecida Miranda nos apresenta uma reflexão importante sobre temas sensíveis e necessários na atualidade. A autora transita por dois universos ricos, iniciando pela sua história de vida, com momentos de vulnerabilidade, de fé, de coragem, de resiliência e de muito amor, e o das reflexões sobre as inúmeras mudanças que se deram na sociedade contemporânea para a vivência das mulheres.
+      </p>     
+      <br>
+      <p>        
+        <strong>
+          Como adquirir:
+        </strong>
+      </p>
+      <p><a href="https://www.gruponovoseculo.com.br/index.php?route=product/product&product_id=2239&search=filha+de+onelia" target="_blank" rel="www.gruponovoseculo.com.br">www.gruponovoseculo.com.br</a>.</p>
+      <p><a href="https://www.amazon.com.br/Filha-On%C3%A9lia-vis%C3%A3o-mulher-contemporaneidade/dp/6555614331/ref=sr_1_1?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=2ZK9E61019M8I&keywords=a+filha+de+onelia&qid=1672317321&sprefix=a+filha+de+oneli%2Caps%2C281&sr=8-1" target="_blank" rel="www.amazon.com.br">www.amazon.com.br</a>.</p>
+      <p><a href="https://linktr.ee/aparecidamiranda" target="_blank" rel="linktr">linktr</a>.</p>
+      <br>
+    </div>
+    <div class="line"></div>
+    <div class="footer">
+      <h2>Contatos</h2>
+      <div>
+        <ul>
+          <li>
+            <a href="https://linktr.ee/aparecidamiranda" target="_blank"
+              ><i class="fa-brands fa-whatsapp fa-2xl"></i
+            ></a>
+          </li>
+          <li>
+            <a
+              href="https://instagram.com/aparecidamirandapsicologa?utm_medium=copy_link"
+              target="_blank"
+              ><i class="fa-brands fa-instagram fa-2xl"></i
+            ></a>
+          </li>
+          <!-- <li>
+            <a href="#" ><i class="fa-brands fa-linkedin fa-2xl"></i></a>
+          </li> -->
+        </ul>
+      </div>
+    </div>
+    <div class="line"></div>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: "HomeView"
+  name: "HomeView",
 };
 </script>
 
 <style>
+
 .foto {
   height: 38%;
   width: 38%;
   border-radius: 50%;
+  margin-bottom: 30px;
 }
 
+.foto2 {
+  height: 38%;
+  width: 38%;
+  margin-bottom: 30px;
+  padding: 15px;
+}
+
+h1 {
+  color: beige;
+  font-weight: bolder;
+  font-size: xx-large;
+}
+
+h2 {
+  color: beige;
+  font-weight: normal;
+}
+
+p {
+  font-size: medium;
+  margin-bottom: 10px;
+  color: beige;
+  margin-right: 10px;
+}
+
+a {
+  color: gold;
+}
+
+.lista-portfolio {  
+  text-align: left; 
+  margin-left: 30px;
+}
+
+.item-lista-portfolio {
+  list-style: disc;  
+  font-size: medium;
+  margin-bottom: 10px;
+}
+
+.page-title {
+  margin-bottom: 30px;
+}
+
+.home {
+  background: #000 url("../assets/bg.jpg");
+}
+
+.about {    
+    text-align: left;
+    margin-left: 30px;
+}
+
+.book {
+   text-align: left;
+   margin-left: 30px;
+}
+
+.line {
+  height: 10px;
+  background: #000 url("../assets/h.png") repeat top center;
+}
+
+.footer {
+  /* height: 80px;
+  width: 100%;
+  margin-bottom: 30px;
+  background: #000 url("../assets/bg.jpg"); */
+  text-align: left;
+  margin-left: 30px;
+}
+
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+li {  
+  padding: 15px;
+  font-size: x-large;
+  color: beige;
+}
 </style>
